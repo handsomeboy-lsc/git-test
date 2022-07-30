8a4adce51fe78278109b84b58bae158bd6b41dbe
Wangchuan <Wangchuan@lsc.com>
2022-07-30 13:44:07 +0800

    third commit

--- src/main/java/com/lsc/git/GitTest.java
+++ src/main/java/com/lsc/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("Git Test2");
+        System.out.println("Git Test3");
 
     }
 }
