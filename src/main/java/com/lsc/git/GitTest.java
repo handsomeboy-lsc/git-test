2008d3a85a8295edcc2f60d84f4a647e41eadcb5
Wangchuan <Wangchuan@lsc.com>
2022-07-30 13:41:21 +0800

    first commit

---
+++ src/main/java/com/lsc/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.lsc.git;
+
+/**
+ * @author 梁世昌
+ * @create 2022/7/30 13:38
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+
+    }
+}
